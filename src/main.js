2d8f4f171a2ec83619aa0dbf4724d20767df4af4
yangqingjun01 <yangqingjun01@yeah.net>
2021-08-03 22:32:32 +0800

    express初体验

--- src/main.js
+++ src/main.js
@@ -9,3 +9,31 @@ app.listen(port, () =>{
 app.get('/', (request, response) => {
     response.send('你好');
 });
+
+const data = [
+    {
+        id: 1,
+        title: '第一篇',
+        content: '第一篇正文'
+    },
+    {
+        id: 2,
+        title: '第二篇',
+        content: '第二篇正文'
+    },
+    {
+        id: 3,
+        title: '第三篇',
+        content: '第三篇正文'
+    },
+];
+
+app.get('/posts', (request, response) => {
+    response.send(data);
+})
+
+app.get('/posts/:postId', (request, response) => {
+    const { postId } = request.params;
+    const posts = data.filter(item => item.id == postId);
+    response.send(posts[1]);
+})
